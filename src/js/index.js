9dc0024872dbde41c858abb3e1b0a4393b8a03cc
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-01-31 08:45:58 +0000

    Add small adjustment in files

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,20 @@
 const navLinks = document.querySelectorAll('.navigation__link');
 const nav = document.getElementById('navi');
+const courses = document.getElementById('courses-heading');
+
+const popup = document.querySelector('.popup');
 
 Array.from(navLinks).forEach(link =>
   link.addEventListener('click', () => {
     nav.checked = false;
   })
 );
+
+popup.addEventListener('click', (e) => closePopup(e))
+
+const closePopup = (e) => {
+  if (e.target.closest('.popup__content'))
+    return;
+
+  window.location.href = `#${courses}`;
+}
